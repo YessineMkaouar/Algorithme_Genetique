b7d7f0577e16662f35e679ee162a951f151b4cb3
YessineMkaouar <yassine.mkaouar@etudiant-enit.utm.tn>
2023-05-06 02:35:33 +0100

    Add image and style to interface

--- Code/interface.py
+++ Code/interface.py
@@ -2,9 +2,10 @@ import streamlit as st
 from helpers import run_genetic_algorithm,create_file,generate_sequence,calculate_sum
 
 st.write("""
-# Interface Graphique IN104
-
-Veuillez entrer un ensemble de la forme : nbr1 nbr2 nbr3 ...
+# Interface Graphique - Algorithmes Génétiques
+## Projet IN104
+### Problème de la somme des sous-ensembles
+Veuillez entrer un ensemble sous la forme : nbr1 nbr2 nbr3 ...
 
 """)
 user_input = st.text_input("Veuillez entrer votre message :")
@@ -26,3 +27,10 @@ if submit_button:
     st.write("La solution est ")
     st.write(" ".join(solution))
     st.write("fitness = {}".format(fitness))
+
+from PIL import Image
+
+img = Image.open("logo_TA.jpg")
+new_size = (500, 500)
+img = img.resize(new_size)
+st.image(img, caption="My Image", use_column_width=True)
