e9e531c6cd628cc4dece204a1275d194f92e3fc4
Mohamed MKAOUAR <mohamed_mkaouar@carrefour.com>
2023-05-06 03:05:58 +0200

    edit interface to new format

--- Code/helpers.py
+++ Code/helpers.py
@@ -10,3 +10,33 @@ def create_file(L: list, nbr_population: int, nbr_iteration: int):
         f.write(str(nbr_iteration) + "\n")
         f.write(str(len(L)) + "\n")
         f.write(" ".join(map(str, L)))
+
+def generate_sequence(L):
+    res = ''.join(L)
+    res = res.replace('--', '+')  # remplace les '--' par des '+'
+    if res.startswith('+'):  # vérifie si la chaîne commence par '+'
+        res = res[1:]  # supprime le premier caractère s'il est '+'
+    return res
+
+def calculate_sum(L):
+    # Initialiser le signe courant et la somme courante
+    signe = '+'
+    somme = 0
+    
+    # Itérer à travers la liste
+    for item in L:
+        # Vérifier si l'élément est un signe
+        if item in ('+', '-'):
+            # Mettre à jour le signe courant
+            signe = item
+        else:
+            # Convertir l'élément en nombre entier
+            nombre = int(item)
+            # Ajouter ou soustraire de la somme courante en fonction du signe courant
+            if signe == '+':
+                somme += nombre
+            else:
+                somme -= nombre
+    
+    # Renvoyer la somme finale
+    return somme
--- Code/interface.py
+++ Code/interface.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from helpers import run_genetic_algorithm,create_file
+from helpers import run_genetic_algorithm,create_file,generate_sequence,calculate_sum
 
 st.write("""
 # Interface Graphique IN104
@@ -12,13 +12,17 @@ submit_button = st.button("Envoyer")
 
 
 def generate_response(user_input):
-    
+
     L = user_input.split()
     L = [int(x) for x in L]
-    create_file(L = L,nbr_population = 200, nbr_iteration = 300)
+    create_file(L = L,nbr_population = len(L), nbr_iteration = 300)
     response = run_genetic_algorithm()
     return response
 
 if submit_button:
     response = generate_response(user_input)
-    st.write(" ".join(response))
+    solution = generate_sequence(response)
+    fitness = calculate_sum(response)
+    st.write("La solution est ")
+    st.write(" ".join(solution))
+    st.write("fitness = {}".format(fitness))
