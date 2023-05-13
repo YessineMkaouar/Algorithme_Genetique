1307914cf097a29c63f9d3cee33607150ae70e7c
Mohamed MKAOUAR <mohamed_mkaouar@carrefour.com>
2023-05-14 01:15:37 +0200

    add automatic program for interface generation

--- helpers.py
+++ helpers.py
@@ -1,5 +1,16 @@
+def run_command(command):
+    import subprocess
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Command execution failed with error code {e.returncode}: {e.output}")
+
+
 def run_genetic_algorithm():
     import subprocess
+    command = "gcc -o program_for_interface main_for_interface.c utils.c"
+    run_command(command)
     output = subprocess.check_output(["./program_for_interface"]) #Lire l'output du main_interface
     best = output.decode('utf-8').split() #Convertir l'output en liste
     return best
